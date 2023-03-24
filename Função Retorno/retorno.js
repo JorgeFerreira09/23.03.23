1d5ba83f1511c0355b47e38d0a2767f3848ecab7
JorgeFerreira09 <ferreirafilho081993@gmail.com>
2023-03-23 23:50:03 -0300

    first commit

---
+++ Função Retorno/retorno.js
@@ -0,0 +1,18 @@
+/* function calcularMedia (a, b){
+    const media = ( a + b) / 2
+}
+
+const resultado = calcularMedia(7, 2)
+
+console.log(resultado)
+
+*/
+
+function calcularMedia (a, b){
+    const media = ( a + b) / 2
+    return media
+}
+
+const resultado = calcularMedia(7, 2)
+
+console.log(resultado)
